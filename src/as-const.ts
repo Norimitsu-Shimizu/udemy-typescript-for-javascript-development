cc0e74983a2c9faf2c27906f69e8ef9872cbd97d
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-03-01 16:38:55 +0900

    practice ts as consts

---
+++ src/as-const.ts
@@ -0,0 +1,16 @@
+export{} ;
+
+let name = 'Nori';
+
+name = 'Ham';
+
+let nickname = 'Ham' as const;
+nickname = 'Ham';
+
+let profile = {
+  name: 'Nori',
+  height: 180
+} as const;
+
+// profile.name = 'Ham';
+// profile.height = 178;
