1aad541485b38fc7da9b2e2fdabce0a6ce8a5a83
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-03-01 14:51:55 +0900

    practice ts retrun of interface

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+class Mahoutukai {}
+class Souryo {}
+
+class Taro extends Mahoutukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Sensi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Sensi {
+  ionazun(): void {
+    console.log('ionazun');
+  };
+
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
